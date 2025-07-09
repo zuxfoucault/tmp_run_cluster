891287bdf4b5f870e1cdcccda5031a1d3203483e
foucault <zuxfoucault@gmail.com>
2025-07-09 12:37:47 -0400

    update

--- join_head.sh
+++ join_head.sh
@@ -1,12 +1,19 @@
 # head node
-bash ~/startup/run_cluster.sh .cache/vllm-openai_latest.sif balam005 --head $HF_HOME
+export HF_HUB_CACHE=/root/.cache/huggingface
+export VLLM_CACHE_ROOT=/project/a/aspuru/foucault/.cache/vllm2
+apptainer exec --nv -B ${HF_HOME}/hub:/root/.cache/huggingface -B /project/a/aspuru/foucault/.cache -B /project/a/aspuru/foucault/.cache/vllm2 .cache/vllm-openai_latest.sif /bin/bash
+#Apptainer>
+ray start --head --port=6379
+
+# bash ~/startup/run_cluster.sh .cache/vllm-openai_latest.sif balam005 --head $HF_HOME
 
 # run worker node
-apptainer exec --nv -B ${HF_HOME}:/root/.cache/huggingface -B ~ -B ~/project -B ~/.cache .cache/vllm-openai_latest.sif /bin/bash
-
-# in the worker container, joining head
-#Apptainer>
-ray start --address='172.16.30.45:6379'
+export HF_HUB_CACHE=/root/.cache/huggingface
+export VLLM_CACHE_ROOT=/project/a/aspuru/foucault/.cache/vllm2
+apptainer exec --nv -B ${HF_HOME}/hub:/root/.cache/huggingface -B /project/a/aspuru/foucault/.cache -B /project/a/aspuru/foucault/.cache/vllm2 .cache/vllm-openai_latest.sif /bin/bash
 
 # run vllm
-vllm serve "deepseek-ai/DeepSeek-R1-0528-Qwen3-8B" --tensor-parallel-size 4
+#Apptainer>
+ray start --address='172.16.30.45:6379'
+export VLLM_HOST_IP=172.16.30.46
+vllm serve "deepseek-ai/DeepSeek-R1-0528-Qwen3-8B" --tensor-parallel-size 4 --pipeline-parallel-size 2
